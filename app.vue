f7dae023040229f1c76200c8d29821fa75ad4be0
yourname <rugor@users.noreply.github.com>
2022-11-20 13:38:43 -0800

    added routing/pages

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/posts/01">Post 01</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/posts/02">Post 02</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div>
+      <NuxtPage />
+    </div>
   </div>
 </template>
---
+++ pages/posts/01.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- This page correctly has only one single root element -->
+    <p>Post 01 content</p>
+  </div>
+</template>
+
